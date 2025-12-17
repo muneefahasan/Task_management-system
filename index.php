009dc3fed4fc4c300bcbd41c76c599b1631eefca
muneefahasan <muneefahasan@gmail.com>
2025-12-17 17:37:17 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+<body>
+    <input type="checkbox" id="checkbox">
+    <header class="header">
+        <h2 class="u-name">Side<b>BAR</b>
+            <label for="checkbox">
+                <i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
+            </label>
+        </h2>
+        <i class="fa-fa-bell" aria-hidden="true"></i>
+    </header>
+
+    <div class="body">
+        <nav class="side-bar">
+            <div class="user-p">
+                <img src="img/user.png">
+                <h4>@User</h4>
+            </div>
+
+            <?php
+            $user = "admin";
+
+            if ($user == "employee") {
+                ?>
+                <!----employee navigation bar-->
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tachometer" aria-hidden="true"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <span>My Task</span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-bell" aria-hidden="true"></i>
+                            <span>Notification</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-sign-out" aria-hidden="true"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+
+            <?php } else { ?>
+                <!----Admin navigation bar-->
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tachometer" aria-hidden="true"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-users" aria-hidden="true"></i>
+                            <span>Manage Users</span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                            <span>Create Task</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <span>All Tasks</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <span>Notification</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-sign-out" aria-hidden="true"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+
+            <?php } ?>
+
+        </nav>
+        <section class="section-1">
+
+        </section>
+    </div>
+
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Task Mangement System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body class="Login-body">
+
+    <form method="POST" action="app/login.php" class="shadow p-4">
+        <h3 class="display-4">LOGIN</h3>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">User name</label>
+            <input type="text" class="form-control" name="user_name">
+
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
